e24c85c86382c5937465bce366657888d90a314e
Kaviru Nethsara <kavirunethsara1@outlook.com>
2024-09-06 23:18:26 +0530

    Done

--- script.js
+++ script.js
@@ -2,6 +2,17 @@
 
 function rollDice(){
     const numOfDice = document.getElementById("numOfDice").value;
-    const randomeNum = Math.floor(Math.random() * 6 + 1)
-    console.log(randomeNum)
+    const diceTxt = document.getElementById("diceTxt");
+    const diceImages = document.getElementById("diceImages");
+    const value = [];
+    const images = [];
+
+    for(let i=0 ; i < numOfDice; i++){
+        const randomeNum = Math.floor(Math.random() * 6) +1;
+        value.push(randomeNum);
+        images.push(`<img src="images/${randomeNum}.png" alt = "Dice-${randomeNum}">`);
+
+    }
+    diceTxt.textContent = `dice: ${value.join(`, `)}`;
+    diceImages.innerHTML = images.join('');
 }
